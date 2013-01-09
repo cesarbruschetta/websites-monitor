378b782571e540d900c960856fade58cc6d77b3e
Cesar Augusto <cesarbruschetta@hotmail.com>
2013-01-09 20:37:09 -0200

    Adição do botão para gerar grafico

--- static/js/ajax-grafic.js
+++ static/js/ajax-grafic.js
@@ -1,23 +1,27 @@
+function geraGrafico(){
+	var url = "/ajax-grafic"
+	
+	var site = $('select#site').val();
+	var start_date = $('input#start-date').val();
+	var end_date = $('input#end-date').val();
+	
+	$('div#content-grafico').html("");		
+	
+	if (site != ''){ 
+		$.get(url,{site:site,
+				   start_date:start_date,
+				   end_date:end_date}, function(data){
+	
+			$('div#content-grafico').html(data);
+		});
+	};
+};
+
 $(document).ready(function(){
 	$('select#site').change(function(){
-		var url = "/ajax-grafic"
-		
-		var site = $(this).val();
-		var start_date = $('input#start-date').val();
-		var end_date = $('input#end-date').val();
-		
-		if (site != ''){ 
-			$.get(url,{site:site,
-					   start_date:start_date,
-					   end_date:end_date}, function(data){
-	
-						$('div#content-grafico').html(data);
-				});
-		}else{
-			$('div#content-grafico').html("");			
-		}
-				
+		geraGrafico();
 	});
-	
-
+	$('input#gera').click(function(){
+		geraGrafico();
+	});
 });
